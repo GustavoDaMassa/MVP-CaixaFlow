d82ed67079b06d6acf75ba18e8fff9338621b39b
Gustavo <gustavo.pereira@discente.ufg>
2026-05-25 16:41:53 -0300

    fix: skip authenticate_user! for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
   layout :layout_by_resource
 
   private
